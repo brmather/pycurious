ace5710798a640868717e5649772737914ac1b38
Ben Mather <bmather@cp.dias.ie>
2018-01-17 19:28:09 +0000

    Updated routines to calculate radial spectrum

--- curie.py
+++ curie.py
@@ -2,7 +2,7 @@
 import numpy as np
 from scipy.special import gamma, kv, lambertw
 from scipy.optimize import minimize
-from scipy.signal import turkey
+from scipy.signal import tukey
 import time
 
 try: range = xrange
@@ -22,14 +22,15 @@ class CurieGrid(object):
      ymin     : minimum y bound
      ymax     : maximum y bound
     """
-    def __init__(self, data, xmin, xmax, ymin, ymax):
+    def __init__(self, grid, xmin, xmax, ymin, ymax):
         
-        self.data = np.array(data)
-        self.ny, self.nx = self.data.shape
+        self.data = np.array(grid)
+        ny, nx = self.data.shape
         self.xmin, self.xmax = xmin, xmax
         self.ymin, self.ymax = ymin, ymax
         self.xcoords, self.dx = np.linspace(xmin, xmax, nx, retstep=True)
         self.ycoords, self.dy = np.linspace(ymin, ymax, ny, retstep=True)
+        self.nx, self.ny = nx, ny
 
 
     def subgrid(self, xc, yc, window):
@@ -39,15 +40,18 @@ class CurieGrid(object):
         
         Parameters
         ----------
-         xc     : x coordinate
-         yc     : y coordinate
-         window : size of window in metres
+         xc      : x coordinate
+         yc      : y coordinate
+         window  : size of window in metres
 
         Returns
         -------
-         data   : subgrid
-         nw     : size of window in integers
+         data    : subgrid
         """
+
+        # check in coordinate in grid
+        if xc < self.xmin or xc > self.xmax or yc < self.ymin or yc > self.ymax:
+            raise ValueError("Point (xc,yc) outside data range")
 
         # find nearest index to xc,yc
         ix = np.abs(self.xcoords - xc).argmin()
@@ -59,36 +63,36 @@ class CurieGrid(object):
         # extract a square window from the data
         data = self.data[ix-n2w:ix+n2w+1, iy-n2w:iy+n2w+1]
 
-        return data, nw
+        return data
 
-    def radial_spectrum(self, xc, yc, window, taper=np.hanning, scale=0.001, *args):
+    def radial_spectrum(self, subgrid, taper=np.hanning, scale=0.001, *args):
         """
         Compute the radial spectrum for a point (xc,yc)
         for a square window
 
         Parameters
         ----------
-         xc     : x coordinate
-         yc     : y coordinate
-         window : size of window
-         taper  : taper function (np.hanning is default)
-         scale  : scaling factor to get k into rad/km
-                : (0.001 by default)
-         args   : arguments to pass to taper
+         subgrid : window of the original data
+                 : (see subgrid method)
+         taper   : taper function (np.hanning is default)
+                 : set to None for no taper function
+         scale   : scaling factor to get k into rad/km
+                 : (0.001 by default)
+         args    : arguments to pass to taper
 
         Returns
         -------
-         S      : Radial spectrum
-         k      : wavenumber in rad/km
-         sigma2 : variance of S
+         S       : Radial spectrum
+         k       : wavenumber in rad/km
+         sigma2  : variance of S
         """
 
-        # check in coordinate in grid
-        if xc < self.xmin or xc > self.xmax or yc < self.ymin or yc > self.ymax:
-            raise ValueError("Point xc,yc outside data range")
-
-        data, nw = self.subgrid(xc, yc, window)
+        data = subgrid
         nr, nc = data.shape
+        nw = nr
+
+        if nr != nc:
+            raise RuntimeWarning("subgrid is not square {}".format((nr,nc)))
 
         # control taper
         if taper is None:
@@ -130,3 +134,87 @@ class CurieGrid(object):
             sigma2[i] = np.std(rr)/np.sqrt(rr.size)
 
         return S, k, sigma2
+
+
+    def azimuthal_spectrum(self, subgrid, taper=np.hanning, scale=0.001, *args):
+        pass
+
+
+# Helper functions to calculate Curie depth
+
+def bouligand2009(beta, zt, dz, kh, C=0.0):
+    """
+    Calculate the synthetic radial power spectrum of
+    magnetic anomalies
+
+    Equation (4) of Bouligand et al. (2009)
+
+    Parameters
+    ----------
+     beta  : fractal parameter
+     zt    : top of magnetic sources
+     dz    : thickness of magnetic sources
+     kh    : norm of the wave number in the horizontal plane
+     C     : field constant (Maus et al., 1997)
+
+    Returns
+    -------
+     Phi  : radial power spectrum of magnetic anomalies
+
+    References
+    ----------
+     Bouligand, C., J. M. G. Glen, and R. J. Blakely (2009), Mapping Curie
+       temperature depth in the western United States with a fractal model for
+       crustal magnetization, J. Geophys. Res., 114, B11104,
+       doi:10.1029/2009JB006494
+     Maus, S., D. Gordon, and D. Fairhead (1997), Curie temperature depth
+       estimation using a self-similar magnetization model, Geophys. J. Int.,
+       129, 163–168, doi:10.1111/j.1365-246X.1997.tb00945.x
+    """
+    # from scipy.special import kv
+    khdz = kh*dz
+    coshkhdz = np.cosh(khdz)
+
+    Phi1d = C - 2.0*kh*zt - (beta-1.0)*np.log(kh) - khdz
+    A = np.sqrt(np.pi)/gamma(1.0+0.5*beta) * \
+        (0.5*coshkhdz*gamma(0.5*(1.0+beta)) - \
+        kv((-0.5*(1.0+beta)), khdz) * np.power(0.5*khdz,(0.5*(1.0+beta)) ))
+    Phi1d += np.log(A)
+    return Phi1d
+
+
+def maus1995(beta, zt, kh, C=0.0):
+    """
+    Calculate the synthetic radial power spectrum
+    of magnetic anomalies
+
+    Maus and Dimri (1995)
+
+    This is not all that useful except when testing
+    overflow errors which occur for the second term
+    in Bouligand et al. (2009).
+
+    Parameters
+    ----------
+     beta  : fractal parameter
+     zt    : top of magnetic sources
+     dz    : thickness of magnetic sources
+     kh    : norm of the wave number in the horizontal plane
+     C     : field constant (Maus et al., 1997)
+
+    Returns
+    -------
+     Phi  : radial power spectrum of magnetic anomalies
+
+    References
+    ----------
+     Bouligand, C., J. M. G. Glen, and R. J. Blakely (2009), Mapping Curie
+       temperature depth in the western United States with a fractal model for
+       crustal magnetization, J. Geophys. Res., 114, B11104,
+       doi:10.1029/2009JB006494
+     Maus, S., D. Gordon, and D. Fairhead (1997), Curie temperature depth
+       estimation using a self-similar magnetization model, Geophys. J. Int.,
+       129, 163–168, doi:10.1111/j.1365-246X.1997.tb00945.x
+    """
+    khdz = kh*dz
+    return C - 2.0*kh*zt - (beta-1.0)*np.log(kh)
