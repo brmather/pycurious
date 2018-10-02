a9467ef2bca1bb1410945571df04d00b55fde634
Ben Mather <brmather1@gmail.com>
2018-10-02 23:37:09 +0100

    Updated documentation for thermal solver

--- pycurious/therm/__init__.py
+++ pycurious/therm/__init__.py
@@ -17,9 +17,4 @@ along with PyCurious.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 # -*- coding: utf-8 -*-
-from .grid import CurieGrid, bouligand2009, tanaka1999, maus1995, ComputeTanaka
-from .optimise import CurieOptimise
-from .parallel import CurieParallel
-from .therm import CurieTherm
-from . import mapping
-from .documentation import install_documentation
+from .therm import *
