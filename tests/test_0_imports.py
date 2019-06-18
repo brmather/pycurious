c0aa7b9e070c8a4b8e963de85b8bf6bc7d7c45d8
Ben Mather <brmather1@gmail.com>
2019-06-18 14:11:45 +1000

    First lot of tests

---
+++ tests/test_0_imports.py
@@ -0,0 +1,43 @@
+import pytest
+
+## ===================
+
+def test_numpy_import():
+    import numpy
+    return
+
+def test_scipy_import():
+    import scipy
+    print("\t\t You have scipy version {}".format(scipy.__version__))
+
+def test_cython_import():
+	import Cython
+	return
+
+def test_pycurious_modules():
+    import pycurious
+    from pycurious import documentation
+    from pycurious import CurieGrid
+    from pycurious import CurieOptimise
+    from pycurious import mapping
+    from pycurious import download
+
+
+# def test_jupyter_available():
+#     from subprocess import check_output
+#     try:
+#         result = str(check_output(['which', 'jupyter']))[2:-3]
+#     except:
+#         assert False, "jupyter notebook system is not installed"
+
+# def test_documentation_dependencies():
+#     import matplotlib
+#     import cartopy
+#     import pyproj
+
+
+if __name__ == "__main__":
+    test_numpy_import()
+    test_scipy_import()
+    test_cython_import()
+    test_pycurious_modules()
