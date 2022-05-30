695c852fa218f8ecf459ba778b7111f608114094
Ben Mather <brmather1@gmail.com>
2022-05-30 15:36:24 +1000

    restructure repo and bring setup.py up to date

--- setup.py
+++ setup.py
@@ -11,35 +11,42 @@
 ##
 ## (see http://peterdowns.com/posts/first-time-with-pypi.html)
 
-
-import numpy as np
-from setuptools import setup, Extension
-from Cython.Build import cythonize
+from setuptools import dist, setup, find_packages
 from os import path
 import io
+import os
+import subprocess
+import platform
+
+links_args = []
+ 
+if "Windows" in platform.system():
+    link_args = ["-static"]
+
 
 ## in development set version to none and ...
-PYPI_VERSION = "1.1.1"
+PYPI_VERSION = "2.0"
 
 # Return the git revision as a string (from numpy)
 def git_version():
+    
     def _minimal_ext_cmd(cmd):
         # construct minimal environment
         env = {}
-        for k in ["SYSTEMROOT", "PATH"]:
+        for k in ['SYSTEMROOT', 'PATH']:
             v = os.environ.get(k)
             if v is not None:
                 env[k] = v
         # LANGUAGE is used on win32
-        env["LANGUAGE"] = "C"
-        env["LANG"] = "C"
-        env["LC_ALL"] = "C"
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
         return out
 
     try:
-        out = _minimal_ext_cmd(["git", "rev-parse", "--short", "HEAD"])
-        GIT_REVISION = out.strip().decode("ascii")
+        out = _minimal_ext_cmd(['git', 'rev-parse', '--short', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
     except OSError:
         GIT_REVISION = "Unknown"
 
@@ -54,45 +61,33 @@ with open(path.join(this_directory, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
 
 
-ext = Extension(
-    name="pycurious.radon",
-    sources=["src/radon.pyx", "src/cradon.c"],
-    include_dirs=[np.get_include(), "src"],
-    library_dirs=["pycurious"],
-    extra_compile_args=["-std=c99"],
-)
-
 if __name__ == "__main__":
-    setup(
-        name="pycurious",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        author="Ben Mather",
-        author_email="brmather1@gmail.com",
-        url="https://github.com/brmather/pycurious",
-        version=PYPI_VERSION,
-        description="Python tool for computing the Curie depth from magnetic data",
-        ext_modules=cythonize([ext]),
-        install_requires=["numpy", "scipy>=0.15.0", "Cython>=0.25.2"],
-        python_requires=">=2.7, >=3.5",
-        setup_requires=["pytest-runner", "webdav"],
-        tests_require=["pytest", "webdav"],
-        packages=["pycurious"],
-        package_data={
-            "pycurious": [
-                "Examples/data/test_mag_data.txt",
-                "Examples/*.ipynb",
-                "Examples/Notebooks/Tanaka/*.ipynb",
-                "Examples/Notebooks/Bouligand/*.ipynb",
-                "Examples/Scripts/*.py",
-            ]
-        },
-        classifiers=[
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-        ],
-    )
+    setup(name = 'pycurious',
+          author            = "Ben Mather",
+          author_email      = "brmather1@gmail.com",
+          url               = "https://github.com/brmather/pycurious",
+          version           = PYPI_VERSION,
+          description       = "Python tool for computing the Curie depth from magnetic data",
+          long_description  = long_description,
+          long_description_content_type='text/markdown',
+          install_requires  = ['numpy>=1.16.0',
+                               'scipy>=1.0.0',
+                               ],
+          packages          = ['pycurious'],
+          package_data      = {'pycurious': ['Examples/*ipynb', # Worked Examples is not currently used
+                                             'Examples/data/*'] },
+          include_package_data = True,
+          classifiers       = ['Programming Language :: Python :: 2',
+                               'Programming Language :: Python :: 2.6',
+                               'Programming Language :: Python :: 2.7',
+                               'Programming Language :: Python :: 3',
+                               'Programming Language :: Python :: 3.3',
+                               'Programming Language :: Python :: 3.4',
+                               'Programming Language :: Python :: 3.5',
+                               'Programming Language :: Python :: 3.6',
+                               'Programming Language :: Python :: 3.7',
+                               'Programming Language :: Python :: 3.8',
+                               'Programming Language :: Python :: 3.9',
+                               'Programming Language :: Python :: 3.10'
+                               ]
+          )
